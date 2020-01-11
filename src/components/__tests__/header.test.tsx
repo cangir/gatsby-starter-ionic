a5bfba7121a2566734f06cad654e20573a7194d7
Barry Low <barry@studiobear.co>
2020-01-10 18:04:35 -0800

    Add husky git hooks

--- src/components/__tests__/header.test.tsx
+++ src/components/__tests__/header.test.tsx
@@ -1,12 +1,16 @@
-import * as React from "react"
+import * as React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from "@testing-library/react"
+import { render } from '@testing-library/react'
 
-import Header from "../header"
+import Header from '../header'
 
-describe("Header", () => {
-  it("renders without crashing", () => {
-    const { getByText } = render(<Header siteTitle="Gatsby Starter Ionic" />);
-    expect(getByText("Gatsby Starter Ionic")).toBeInTheDocument();
+describe('Header', () => {
+  it('renders without crashing', () => {
+    const { getByText } = render(<Header siteTitle="Gatsby Starter Ionic" />)
+    expect(getByText('Gatsby Starter Ionic')).toBeInTheDocument()
+  })
+  it('render two nav buttons', () => {
+    const { getByText } = render(<Header siteTitle="Gatsby Starter Ionic" />)
+    expect(queryAllByTestId('todo-item').length).toBe(2)
   })
 })
